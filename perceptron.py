da36f7717af211d177991c2fcbf6a69eadcbfbc6
mcorberas <54457650+mcorberas@users.noreply.github.com>
2019-08-23 18:15:20 -0500

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -2,7 +2,10 @@ import numpy as np
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
-training_input = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
+def sigmoid_derivative(x):
+	return x * (1 -x)
+
+training_inputs = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
 
 training_outputs = np.array([[0,1,1,0]]).T
 
@@ -12,9 +15,14 @@ synaptic_weights = 2 * np.random.random((3, 1)) - 1
 
 print('Random starting synaptic weights: ')
 print(synaptic_weights)
-for iteration in range(1):
+for iteration in range(20000):
 	input_layer = training_inputs
 	outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+	error = training_outputs - outputs
+	adjustments = error * sigmoid_derivative(outputs)
+	synaptic_weights += np.dot(input_layer.T, adjustments)
+print('Synaptic weights afer training: ')
+print(synaptic_weights)
 
 print('Outputs after training: ')
 print(outputs)
