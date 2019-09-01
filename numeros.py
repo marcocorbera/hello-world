8f9c811f67d10cb2608f1bb195d9e0b31ce1a9e3
mcorberas <54457650+mcorberas@users.noreply.github.com>
2019-09-01 03:09:48 -0500

    Create numeros.py

---
+++ numeros.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import numpy as np
+mnist = tf.keras.datasets.mnist #28x28 images of hand-written digits 0-9
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=3)
+
+model.save('epic_num_reader.model')
+new_model = tf.keras.models.load_model('epic_num_reader.model')
+predictions = new_model.predict(x_test)
+print(np.argmax(predictions[5000]))
+plt.imshow(x_test[5000])
+plt.show
